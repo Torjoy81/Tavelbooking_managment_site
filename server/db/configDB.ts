bf8d6c1e2545e90bfb51ff1faa3d92b832b52209
Tarek <islamtarek4@gmail.com>
2023-06-20 17:40:52 +0200

    try to deploy

--- server/db/configDB.ts
+++ server/db/configDB.ts
@@ -6,7 +6,9 @@ import { PrismaClient } from "@prisma/client";
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
+  prisma = new PrismaClient({
+    log: ["info"],
+  });
 } else {
   if (!global.prisma) {
     global.prisma = new PrismaClient();
